9c993e24e505e58cd9575ac36f1e1828dc4e3098
kadia5 <kdabraeux@gmail.com>
2021-07-22 01:55:28 +0000

    eqObjects.js

---
+++ eqObjects.js
@@ -0,0 +1,57 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function (arg1, arg2) {
+  if (arg1.length !== arg2.length) {
+    return false
+  }
+  for (let i = 0; i < arg1.length; i++) {
+    if (arg1[i] != arg2[i]) {
+      return false
+    }
+  }
+  return true;
+}
+
+// const sum = function(a, b) {
+//   return a + b;
+// };
+// // TEST CODE
+// console.log(assert(sum(1, 2) === 3));
+// console.assert(sum(1, 20) === 3); // bad / incorrect assertion, and we see i
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
+    console.log(`Assertion failed: ${actual} !== ${expected}`);
+  }
+
+};
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function (object1, object2) {
+
+  let arr1 = Object.keys(object1);
+  let arr2 = Object.keys(object2);
+  console.log("lights", arr1);
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (const key of arr1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// // TEST CODE
+// console.log(assertEqual("Lighthouse Labs"+ "Bootcamp"));
+// console.log(assertEqual(1, 1));
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
