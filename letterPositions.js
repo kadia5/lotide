9a51bc67b98fcd63dd70708123e8ffab40a55f20
kadia5 <kdabraeux@gmail.com>
2021-07-25 21:02:29 +0000

    letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,48 +1,60 @@
 // FUNCTION IMPLEMENTATION
-const eqArrays = function(arg1, arg2) { 
-  if (arg1.length !== arg2.length ) {
+const eqArrays = function (arg1, arg2) {
+  if (arg1.length !== arg2.length) {
     return false
   }
-  for(let i = 0; i < arg1.length; i++ ){
-    if (arg1[i] != arg2[i] ) {
+  for (let i = 0; i < arg1.length; i++) {
+    if (arg1[i] != arg2[i]) {
       return false
-    } 
+    }
   }
   return true;
 }
 
-const assertEqual = function(actual, expected) {
-  if(actual === expected){
-console.log(`Assertion Passed: ${actual} === ${expected}`);
+const assertEqual = function (actual, expected) {
 
-  }else{
+  // passed eq arrays into if statement so arguments are passed down
+  if (eqArrays(actual, expected)) {
+
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
     console.log(`Assertion failed: ${actual} !== ${expected}`);
-      }
-  
-};
-const letterPositions = function(sentence) {
-  let arr1 = []
-  let arr2 =[]
-  let arr3 =[]
-  let arr4 =[]
-  let arr5 =[]
-  console.log(sentence)
-
-  for (let value of sentence) {
-    if (value === "hello") {
-      return letterPositions.indexOf(a);
-    
   }
-  arr1.push("hello")
-  arr2.push("hello")
-  arr3.push("hello")
-  arr4.push("hello")
-  arr5.push("hello")  
-}
-}
+
+};
+
+const letterPositions = function (sentence) {
+
+  // let arr = []
+  let result = {}
+  for (let i = 0; i < sentence.length; i++) {
+    // i = 0,1,2,3, etc but sentence[i] = any letter in the loop/that item in that index
+    if (sentence[i] === " ") {
+      continue
+
+    } else if (!result[sentence[i]]) {
+      result[sentence[i]] = [i]
+
+      //   // console.log(letterPositions.push(sentence[i]))
+
+    } else {
+      result[sentence[i]].push(i)
+      // }
+      //blue [ ] access key/create one thats sentence[i] - specify
+      // i makes things dynamic, # specifies a specific letter posit.
+      // result[sentence[i]] = [i]
+    }
+  }
+  return result
+
+};
+console.log(letterPositions("lighthouse labs"))
+assertEqual(letterPositions("lighthouse in the house").i, [1, 11]);
+
+
 // const result1 = letterPositions("hello");
 // console.log(result1)
-console.log(assertEqual(letterPositions("hello").e,[1]));
 
 // assertEqual(result1["l"], 2);
 // assertEqual(result1["i"], 1);
